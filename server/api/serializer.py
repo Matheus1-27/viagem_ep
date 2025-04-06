fe5ec9185d84392b0bd169eae8b2005d1fa176bd
matieus <matieus1344@gmail.com>
2025-04-06 12:27:29 -0300

    apagado

--- server/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Cadastro
-
-class CadastroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cadastro
-        fields = '__all__'
